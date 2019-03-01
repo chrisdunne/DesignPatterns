3dd8c85ec74e1158fd66092a99e452d214d2de85
chris <chrisdunne88@outlook.com>
2019-03-01 22:11:28 +0000

    initial commit - with singleton pattern example and corresponding test fixture.

---
+++ DesignPatterns.Tests/Singleton.cs
@@ -0,0 +1,27 @@
+﻿using DesignPatterns.Singleton;
+using NUnit.Framework;
+
+namespace DesignPatterns.Tests
+{
+    [TestFixture]
+    public class Singleton
+    {
+        [Test]
+        public void WhenAcquiringTwoLoggerInstances_CheckBothReferToTheSameInstance()
+        {
+            var loggerA = Logger.GetInstance();
+
+            loggerA.Message = "First Instance";
+
+            var loggerB = Logger.GetInstance();
+
+            Assert.That(loggerA.Message, Is.EqualTo(loggerB.Message));
+            Assert.That(loggerB.Message, Is.EqualTo("First Instance"));
+
+            loggerB.Message = "Second Instance";
+
+            Assert.That(loggerA.Message, Is.EqualTo(loggerB.Message));
+            Assert.That(loggerA.Message, Is.EqualTo("Second Instance"));
+        }
+    }
+}
---
+++ DesignPatterns/Singleton/Logger.cs
@@ -0,0 +1,24 @@
+﻿namespace DesignPatterns.Singleton
+{
+    public class Logger
+    {
+        private static Logger _logger;
+
+        private Logger()
+        {
+            
+        }
+
+        public string Message { get; set; }
+
+        public static Logger GetInstance()
+        {
+            if (_logger == null)
+            {
+                _logger = new Logger();
+            }
+
+            return _logger;
+        }
+    }
+}
