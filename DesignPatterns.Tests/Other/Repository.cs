c20209e9bc8eac7b30a070808850459fee9b0fa5
chris <contact@chrisdunne.net>
2019-05-29 03:50:48 +0100

    Setup repository test fixture

---
+++ DesignPatterns.Tests/Other/Repository.cs
@@ -0,0 +1,41 @@
+﻿using DesignPatterns.Other.Repository;
+using NUnit.Framework;
+
+namespace DesignPatterns.Tests.Other
+{
+    [TestFixture]
+    public class Repository
+    {
+        private IProductRepository _repository;
+
+        public Repository()
+        {
+            _repository = new CacheProductRepository();
+        }
+
+        [Test]
+        public void WhenGetProductById_ReturnProduct()
+        {
+        }
+
+        [Test]
+        public void WhenGetAllProducts_ReturnIEnummerableOfProducts()
+        {
+        }
+
+        [Test]
+        public void WhenProductAdded_CheckAddedToIEnummerable()
+        {
+        }
+
+        [Test]
+        public void WhenProductDeleted_CheckRemovedFromIEnummerable()
+        {
+        }
+
+        [Test]
+        public void WhenProductUpdated_CheckGetProductReturnsNewProductValues()
+        {
+        }
+    }
+}
